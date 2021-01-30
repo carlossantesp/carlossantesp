ec3c4bf83152cfdd168454420c8ffa62db1df5e5
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-01-30 14:36:47 -0400

    Agregando los iconos necesarios

---
+++ src/components/icons/Times.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Times = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.41427 4.33576L4.33502 5.41501L10.9223 12L4.33502 18.5858L5.41502 19.665L12 13.0778L18.5858 19.6643L19.6643 18.5858L13.0778 12L19.6643 5.41426L18.585 4.33501L12 10.9223L5.41427 4.33576Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
+
+export default Times;
---
+++ src/components/icons/services/Development.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Development = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.7667 4.06667L11.556 27.2427L14.132 27.9333L20.3427 4.75733L17.7667 4.06667Z"
+        fill="black"
+      />
+      <path
+        d="M10.1493 24.48L11.9627 22.5493L11.9613 22.524L5.16933 16.1507L11.556 9.344L9.612 7.51867L1.39867 16.2693L10.1493 24.48Z"
+        fill="black"
+      />
+      <path
+        d="M21.8507 24.48L20.0373 22.5493L20.0387 22.524L26.8307 16.1507L20.444 9.344L22.388 7.51867L30.6013 16.2693L21.8507 24.48Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
+
+export default Development;
