a9333ed30a7ebb2a160d5816173e274d166b198b
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-01 00:01:50 -0400

    Fix: Head component meta etiquetas

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,15 +5,12 @@ const Head = () => {
   return (
     <Helmet>
       <title>Carlos Santillan - Frontend Developer</title>
+      <base href="https://carlos.santillan.dev"></base>
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
       <meta
         name="description"
         content="I am a web developer with more than 2 years of experience in frontend, I am passionate about designing and implementing responsive websites for each device, following the mobile-first methodology."
-      />
-      <meta
-        name="image"
-        content={`https://carlos.santillan.dev${
-          require("../images/projects/portfolio-web.png").default
-        }`}
       />
 
       <meta
@@ -30,8 +27,13 @@ const Head = () => {
           require("../images/projects/portfolio-web.png").default
         }`}
       />
+      <meta
+        property="og:image:alt"
+        content="Portafolio web - Carlos Santillan"
+      />
       <meta property="og:url" content="https://carlos.santillan.dev" />
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Dev Carlos Santillan" />
     </Helmet>
   );
 };
